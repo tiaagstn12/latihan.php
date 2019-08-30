97ba5b86740dd1806c87c3aec3070c4ca9ed0dbd
tiaagstn12 <tiaa7970@gmial.com>
2019-08-30 16:57:43 +0700

    tambah

--- formarraypro.php
+++ formarraypro.php
@@ -2,7 +2,7 @@
 if(isset($_POST['sbn'])){
     $nama = $_POST['nama'];
     $kelas = $_POST['kelas'];
-
+    
     foreach ($nama as $data => $val){
         echo "Nama Saya : ".$nama[$data];
         " kelas : ".$kelas[$data]. "<hr>";
